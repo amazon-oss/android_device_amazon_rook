7f109b000d22d98a07ae1e775366863e70374008
bengris32 <bengris32@protonmail.ch>
2025-09-28 01:24:49 +0100

    rook: Configure Audio stack
    Change-Id: I2602ec2b3443ea64b9c035df85bf338e5c4dbdd4
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -5,6 +5,10 @@
 
 $(call inherit-product, frameworks/native/build/tablet-7in-xhdpi-2048-dalvik-heap.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml
+
 # Screen
 TARGET_SCREEN_DENSITY := 160
 TARGET_SCREEN_HEIGHT := 480
