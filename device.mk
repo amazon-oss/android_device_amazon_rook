830ea3fd8d841baf3b5ed0908ba5f628f0ca7970
Roshan Pius <rpius@google.com>
2025-09-29 15:21:53 +0100

    rook: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5

--- device.mk
+++ device.mk
@@ -37,6 +37,9 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf \
     libwifi-hal
 
+PRODUCT_PACKAGES += \
+    WifiResOverlayRook
+
 PRODUCT_COPY_FILES += \
     hardware/broadcom/wlan/bcmdhd/config/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     hardware/broadcom/wlan/bcmdhd/config/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf
