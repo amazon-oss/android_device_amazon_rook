25460b02e5750b2e160f8327087e61946c65cbf2
bengris32 <bengris32@protonmail.ch>
2025-10-03 12:27:05 +0100

    rook: Import stock thermal policy config
    Change-Id: I43edacae76a05a55c1ac5477348ba84cf66594c1
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -46,6 +46,10 @@ PRODUCT_SHIPPING_API_LEVEL := 22
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal.policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/.tp/thermal.policy.conf
+
 # Vendor partition
 TARGET_HAS_VENDOR_PARTITION := false
 
